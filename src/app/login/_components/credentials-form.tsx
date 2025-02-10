6367bcde742307ae0bce026da53e9bc3941bdf9e
Albert Wales <41163950+AlbertBW@users.noreply.github.com>
2025-02-10 18:38:10 +0000

    enhance login forms with improved styling

--- src/app/login/_components/credentials-form.tsx
+++ src/app/login/_components/credentials-form.tsx
@@ -3,18 +3,30 @@ import { signInWithCredentialsAction } from "@/actions/sign-in.action";
 export default function CredentialsForm() {
   return (
     <form
-      className="flex flex-col justify-center items-center"
+      className="flex flex-col justify-center items-center w-full gap-2"
       action={signInWithCredentialsAction}
     >
-      <label htmlFor="email">
-        Email
-        <input name="email" id="email" />
-      </label>
-      <label htmlFor="password">
-        Password
-        <input name="password" id="password" />
-      </label>
-      <input type="submit" value="Sign In" />
+      <div className="flex flex-col gap-2 w-full">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          className={`mt-1 block w-full rounded-md border shadow-sm p-1 bg-white dark:bg-black dark:text-white border-gray-300 dark:border-gray-600`}
+        />
+      </div>
+      <div className="flex flex-col gap-2 w-full">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          className={`mt-1 block w-full rounded-md border shadow-sm p-1 bg-white dark:bg-black dark:text-white border-gray-300 dark:border-gray-600`}
+        />
+      </div>
+      <input type="submit" value="Sign In" className="mt-4" />
     </form>
   );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,15 +10,29 @@ export default function SignInPage(props: {
   const searchParams = use(props.searchParams);
   return (
     <div className="flex flex-col gap-2 justify-center items-center h-screen">
-      <CredentialsForm />
-      {Object.values(providerMap).map((provider) => (
-        <ProviderForm
-          key={provider.id}
-          provider={provider}
-          searchParams={searchParams}
-        />
-      ))}
-      <BackButton />
+      <div className="min-w-80 w-full sm:w-96 flex justify-start p-6 pb-2">
+        <BackButton />
+      </div>
+      <div className="min-w-80 w-full sm:w-96 border rounded-md border-zinc-600">
+        <div className="text-2xl font-semibold p-6">Sign up</div>
+        <div className="flex flex-col gap-2 p-6 pt-0">
+          {Object.values(providerMap).map((provider) => (
+            <ProviderForm
+              key={provider.id}
+              provider={provider}
+              searchParams={searchParams}
+            />
+          ))}
+          <div>
+            <div className="relative py-2">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t"></span>
+              </div>
+            </div>
+          </div>
+          <CredentialsForm />
+        </div>
+      </div>
     </div>
   );
 }
